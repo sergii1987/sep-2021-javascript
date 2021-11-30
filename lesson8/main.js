7cd7818bd890f7dbd7dd2d0416c0f5759abe9ea4
sergii1987 <asasin066@gmail.com>
2021-11-30 17:05:23 +0200

    homework 8.1

---
+++ lesson8/main.js
@@ -0,0 +1,76 @@
+// Візьміть файл template1.html, підключіть до нього скрипт, і працюйте в ньому!
+
+// - Напишіть код,  який за допомоги document.getElementById або document.getElementsByClassName або document.getElementsByTagName :
+
+// -- отримує текст з параграфа з id "content"
+
+    // let contentElement = document.getElementById('content');
+    // console.log(contentElement);
+
+// -- отримує текст з блоку з id "rules"
+
+    // let rulesElement = document.getElementById('rules');
+    // console.log(rulesElement);
+
+
+
+// -- замініть текст параграфа з id 'content' на будь-який інший
+
+    // let contentElement = document.getElementById('content');
+    // console.log(contentElement);
+    // contentElement.innerText = 'I study at okten school';
+
+// -- замініть текст параграфа з id 'rules' на будь-який інший
+
+    // let rulesElement = document.getElementById('rules');
+    // console.log(rulesElement);
+    // rulesElement.innerText = 'I love okten school'
+
+// -- змініть кожному елементу колір фону на червоний
+//
+//     let contentElement = document.getElementById('content');
+//     console.log(contentElement);
+//     contentElement.style.background = 'red';
+//
+//     let rulesElement = document.getElementById('rules');
+//     console.log(rulesElement);
+//     rulesElement.style.background = 'red';
+//
+//     let ulCollection = document.getElementsByTagName('ul');
+//     for (const fc_rules of ulCollection) {
+//         console.log(ulCollection);
+//         fc_rules.style.background = 'red';
+//     }
+
+
+// -- змініть кожному елементу колір тексту на синій
+
+    // let contentElement = document.getElementById('content');
+    // console.log(contentElement);
+    // contentElement.style.color ='blue';
+    //
+    // let rulesElement = document.getElementById('rules');
+    // console.log(rulesElement);
+    // rulesElement.style.color = 'blue';
+    //
+    // let ulCollection = document.getElementsByTagName('ul');
+    // for (const fc_rules of ulCollection) {
+    //     console.log(ulCollection);
+    //     fc_rules.style.color = 'blue';
+    // }
+
+// -- отримати весь список класів елемента з id=rules і вивести їх в console.log
+
+// let fcbps = document.getElementsByClassName('fc bp');
+// console.log(fcbps);
+
+// -- поміняти колір тексту у всіх елементів fc_rules на червоний
+
+    // let ulCollection = document.getElementsByTagName('ul');
+    // for (const fc_rules of ulCollection) {
+    //     console.log(ulCollection);
+    //     fc_rules.style.color = 'red';
+    // }
+
+
+
