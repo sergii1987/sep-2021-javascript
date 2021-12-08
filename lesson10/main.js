b8dece0427a258d368e0593bcc3ac85c20b7f197
sergii1987 <asasin066@gmail.com>
2021-12-08 18:00:58 +0200

    homework 10.1

---
+++ lesson10/main.js
@@ -0,0 +1,34 @@
+// - Создать произвольный елемент с id = text.  Используя JavaScript, сделайте так, чтобы при клике на кнопку исчезал элемент с id="text".
+
+    // let buttonElement = document.getElementById('button');
+    // buttonElement.onclick = function () {
+    //     document.getElementById('text').style.display = 'none';
+    // }
+
+// - Создайте кнопку, при клике на которую, она будет скрывать сама себя.
+
+    // let buttonElement = document.getElementById('button');
+    // buttonElement.onclick = function () {
+    //     this.style.display = 'none';
+    // }
+
+// - створити інпут який приймає вік людини та кнопку яка підтверджує дію.При натисканні на кнопку зчитати інформацію з інпуту та перевірити вік чи меньше він ніж 18, та повідомити про це користувача
+
+    // let age = document.forms;
+    // age.onsubmit = function (e) {
+    //     e.preventDefault();
+    //     if (+this.userAge.value < 18) {
+    //         alert('You are under eighteen');
+    //     }
+    // };
+
+// - Создайте меню, которое раскрывается/сворачивается при клике
+
+     // let menuElement = document.getElementsByClassName('menu')[0];
+     // let titleElement = menuElement.getElementsByClassName('title')[0];
+     //     titleElement.onclick = function () {
+     //         menuElement.classList.toggle('hidden');
+     //     };
+
+
+
