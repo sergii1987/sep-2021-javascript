1711f15c9d78ea74bdc6525c6a040a051c6a8c14
sergii1987 <asasin066@gmail.com>
2021-12-10 16:29:00 +0200

    homework 11.1

---
+++ lesson11/main.js
@@ -0,0 +1,27 @@
+// -створити форму з інпутами для name та age.
+// При відправці форми записати об'єкт в localstorage
+//
+//     let inputs = document.getElementsByTagName('input');
+//     let name = inputs[0];
+//     let age = inputs[1];
+//     let btn = document.getElementsByTagName('button')[0];
+//     btn.onclick = function () {
+//         let nameUser = name.value;
+//         let ageUser = age.value;
+//         localStorage.setItem('userData',JSON.stringify({name:nameUser , age:ageUser}));
+//     }
+
+// -створити форму з інпутами для model,type та volume автівки.
+//     при відпарвці форми об'єкти зберігаються в масиві в локальному сховищі.
+
+    // let inputs = document.getElementsByTagName('input');
+    // let model = inputs[0];
+    // let type = inputs[1];
+    // let volume = inputs[2];
+    // let btn = document.getElementsByTagName('button')[0];
+    // btn.onclick = function () {
+    //     let modelCar = model.value;
+    //     let typeCar = type.value;
+    //     let volumeCar = volume.value;
+    //     localStorage.setItem('userData',JSON.stringify({model:modelCar,type:typeCar,volume:volumeCar}));
+    // }
